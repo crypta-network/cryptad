84b7505c0c9a80fb1b4ec9eb8f72bf94b841414a
Leumor <116955025+leumor@users.noreply.github.com>
2025-07-29 19:47:33 +0100

    Fix l10n unit tests

--- test/network/crypta/support/TestProperty.java
+++ test/network/crypta/support/TestProperty.java
@@ -12,9 +12,10 @@ final public class TestProperty {
     final public static boolean VERBOSE = Boolean.getBoolean("test.verbose");
     final public static boolean EXTENSIVE = Boolean.getBoolean("test.extensive");
     final public static String L10nPath_test =
-        System.getProperty("test.l10npath_test", "test/freenet/l10n/");
+            System.getProperty("test.l10npath_test", "test/network/crypta/l10n/");
     final public static String L10nPath_main =
-        System.getProperty("test.l10npath_main", "src/freenet/l10n/");
+            System.getProperty("test.l10npath_main", "src/network/crypta/l10n/");
+
     private TestProperty() {
     }
 
