e0d164da4f81ab4da49df6114bf4505787388303
Leumor <116955025+leumor@users.noreply.github.com>
2025-10-18 12:27:58 +0000

    style(spotless): reformat sources via Spotless

--- src/main/java/network/crypta/io/xfer/BlockTransmitter.java
+++ src/main/java/network/crypta/io/xfer/BlockTransmitter.java
@@ -470,7 +470,7 @@ public class BlockTransmitter {
 
   private void sendAllSentNotification() {
     try {
-      _usm.send(_destination, DMT.createAllSent(_uid, realTime), _ctr);
+      _usm.send(_destination, DMT.createAllSent(_uid), _ctr);
     } catch (NotConnectedException e) {
       LOG.info("disconnected for allSent()");
     }
