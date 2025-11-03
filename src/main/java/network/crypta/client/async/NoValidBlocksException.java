ad9a6d2f00e09b614bcd2ae7cbcc8f6ddaa3f9d2
leumor <116955025+leumor@users.noreply.github.com>
2025-11-03 19:20:10 +0100

    chore(client): remove unused NoValidBlocksException\n\n- Confirmed no references across codebase\n- Ran spotlessApply and test suite locally (#377)

--- src/main/java/network/crypta/client/async/NoValidBlocksException.java
+++
@@ -1,8 +0,0 @@
-package network.crypta.client.async;
-
-import java.io.Serial;
-
-public class NoValidBlocksException extends Exception {
-
-  @Serial private static final long serialVersionUID = 1056057448877395180L;
-}
