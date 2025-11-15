8bdfee5f2f8646afc7e68b64a8adfc649884fe9e
Leumor <116955025+leumor@users.noreply.github.com>
2025-11-15 15:45:25 +0000

    docs: expand ProbeBandwidth Javadoc and add tests

---
+++ src/test/java/network/crypta/clients/fcp/ProbeBandwidthTest.java
@@ -0,0 +1,43 @@
+package network.crypta.clients.fcp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import network.crypta.support.SimpleFieldSet;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("java:S100")
+class ProbeBandwidthTest {
+
+  @Test
+  void constructor_whenIdentifierProvided_populatesIdentifierAndBandwidthField() {
+    String identifier = "req-123";
+    float outputBandwidth = 1024.25f;
+
+    ProbeBandwidth response = new ProbeBandwidth(identifier, outputBandwidth);
+
+    SimpleFieldSet fieldSet = response.getFieldSet();
+    assertEquals(identifier, fieldSet.get(FCPMessage.IDENTIFIER));
+    assertEquals(
+        outputBandwidth, Float.parseFloat(fieldSet.get(FCPMessage.OUTPUT_BANDWIDTH)), 0.0001f);
+  }
+
+  @Test
+  void constructor_whenIdentifierNull_doesNotPopulateIdentifierField() {
+    float outputBandwidth = 512.5f;
+
+    ProbeBandwidth response = new ProbeBandwidth(null, outputBandwidth);
+
+    SimpleFieldSet fieldSet = response.getFieldSet();
+    assertNull(fieldSet.get(FCPMessage.IDENTIFIER));
+    assertEquals(
+        outputBandwidth, Float.parseFloat(fieldSet.get(FCPMessage.OUTPUT_BANDWIDTH)), 0.0001f);
+  }
+
+  @Test
+  void getName_whenCalled_returnsProbeBandwidthLiteral() {
+    ProbeBandwidth response = new ProbeBandwidth("any", 1.0f);
+
+    assertEquals("ProbeBandwidth", response.getName());
+  }
+}
