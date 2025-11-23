246b0fbeb7b48ca672100ea6d65640212295a7d0
leumor <116955025+leumor@users.noreply.github.com>
2025-11-23 20:17:49 +0100

    Improve fetch listener docs and add notifier tests (#587)
    * docs: improve FProxyFetchListener javadoc
    
    * test: add NotifierFetchListener test

---
+++ src/test/java/network/crypta/clients/http/updateableelements/NotifierFetchListenerTest.java
@@ -0,0 +1,44 @@
+package network.crypta.clients.http.updateableelements;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@SuppressWarnings("java:S100")
+@ExtendWith(MockitoExtension.class)
+class NotifierFetchListenerTest {
+
+  @Mock private PushDataManager pushManager;
+
+  @Mock private BaseUpdateableElement element;
+
+  @Test
+  void onEvent_whenCalled_updatesElementWithIdFromElement() {
+    String updaterId = "element-123";
+    when(element.getUpdaterId(null)).thenReturn(updaterId);
+    NotifierFetchListener listener = new NotifierFetchListener(pushManager, element);
+
+    listener.onEvent();
+
+    verify(element).getUpdaterId(null);
+    verify(pushManager).updateElement(updaterId);
+    verifyNoMoreInteractions(pushManager, element);
+  }
+
+  @Test
+  void toString_whenCalled_containsManagerAndElementDetails() {
+    when(pushManager.toString()).thenReturn("PushManagerMock");
+    when(element.toString()).thenReturn("ElementMock");
+    NotifierFetchListener listener = new NotifierFetchListener(pushManager, element);
+
+    String result = listener.toString();
+
+    assertEquals("NotifierFetchListener[pushManager:PushManagerMock,element;ElementMock]", result);
+  }
+}
