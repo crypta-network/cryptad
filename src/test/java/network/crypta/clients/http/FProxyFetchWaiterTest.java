f335d6b8e894f4da350e5f8e465be33ffa98557e
Leumor <116955025+leumor@users.noreply.github.com>
2025-11-24 13:54:24 +0000

    test: align getResultFast interaction checks

--- src/test/java/network/crypta/clients/http/FProxyFetchWaiterTest.java
+++ src/test/java/network/crypta/clients/http/FProxyFetchWaiterTest.java
@@ -48,6 +48,8 @@ class FProxyFetchWaiterTest {
     FProxyFetchResult result = waiter.getResultFast();
 
     assertSame(fetchResult, result);
+    verify(progress).finished();
+    verify(progress).hasWaited();
     verify(progress).innerGetResult(false);
     verify(progress, never()).setHasWaited();
     verifyNoMoreInteractions(progress);
